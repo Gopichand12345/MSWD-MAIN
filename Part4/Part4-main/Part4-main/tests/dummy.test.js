35be7b0aace3ddb8364cdb3e0645bce062666511
Gopichand12345 <85008727+Gopichand12345@users.noreply.github.com>
2021-06-13 20:47:14 +0530

    Update dummy.test.js

--- Part4/Part4-main/Part4-main/tests/dummy.test.js
+++ Part4/Part4-main/Part4-main/tests/dummy.test.js
@@ -11,7 +11,7 @@ describe('total likes', () => {
 		{
 			_id: '5a422aa71b54a676234d17f8',
 			title: 'Go To Statement Considered Harmful',
-			author: 'Edsger W. Dijkstra',
+			author: 'Edsger W. Dijkstr',
 			url:
 				'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
 			likes: 5,
@@ -30,7 +30,7 @@ describe('favorite blogs', () => {
 		{
 			_id: '5a422a851b54a676234d17f7',
 			title: 'React patterns',
-			author: 'Michael Chan',
+			author: 'Michel Chan',
 			url: 'https://reactpatterns.com/',
 			likes: 7,
 			__v: 0,
@@ -38,7 +38,7 @@ describe('favorite blogs', () => {
 		{
 			_id: '5a422aa71b54a676234d17f8',
 			title: 'Go To Statement Considered Harmful',
-			author: 'Edsger W. Dijkstra',
+			author: 'Edsger W. Dijkstr',
 			url:
 				'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
 			likes: 5,
@@ -47,7 +47,7 @@ describe('favorite blogs', () => {
 		{
 			_id: '5a422b3a1b54a676234d17f9',
 			title: 'Canonical string reduction',
-			author: 'Edsger W. Dijkstra',
+			author: 'Edsger W. Dijkstr',
 			url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
 			likes: 12,
 			__v: 0,
@@ -85,7 +85,7 @@ describe('favorite blogs', () => {
 		const favorite = {
 			_id: '5a422b3a1b54a676234d17f9',
 			title: 'Canonical string reduction',
-			author: 'Edsger W. Dijkstra',
+			author: 'Edsger W. Dijkstr',
 			url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
 			likes: 12,
 			__v: 0,
